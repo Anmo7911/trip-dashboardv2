2eb5af104b9473c7ace789d385069ba827a8872f
Anmo7911 <officialanmol1609@gmail.com>
2026-08-15 14:20:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src',
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  server: {
-    fs: {
-      strict: false
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        squad: resolve(__dirname, 'squad.html'),
+        route: resolve(__dirname, 'route.html'),
+        chat: resolve(__dirname, 'chat.html'),
+        archives: resolve(__dirname, 'archives.html'),
+        // add any additional HTML files here
+      }
     }
   }
 });
